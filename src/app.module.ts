6595c1ae5f074c0a0ecd2ac7e249a084cd2238b1
David Galisteo <dagalisteo@gmail.com>
2023-07-14 10:10:05 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI)
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+ 
+}
